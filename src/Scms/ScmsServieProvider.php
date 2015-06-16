3ed54ee88e60d242abb0974e3b2cb4134440e784
Andrey Degtyaruk <hlogeon1@gmail.com>
2015-06-16 21:33:14 +0700

    composer fixes

---
+++ src/Scms/ScmsServieProvider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hlogeon
+ * Date: 6/16/15
+ * Time: 9:14 PM
+ */
+
+namespace Hlogeon\Scms;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScmsServieProvider extends ServiceProvider{
+
+
+
+
+}
