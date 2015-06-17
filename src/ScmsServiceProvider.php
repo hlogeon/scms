ba60b2848e4fc2efb76503a47c35dee6f7100b60
Andrey Degtyaruk <hlogeon1@gmail.com>
2015-06-18 04:44:08 +0700

    SleepingOwl Admin Aliases and Service provider

--- src/ScmsServiceProvider.php
+++ src/ScmsServiceProvider.php
@@ -8,6 +8,7 @@
 
 namespace Hlogeon\Scms;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class ScmsServiceProvider extends ServiceProvider{
@@ -34,7 +35,18 @@ class ScmsServiceProvider extends ServiceProvider{
     {
         $this->mergeConfigFrom(__DIR__.'/config/scms.php', 'scms');
         $this->app->register('Cviebrock\EloquentSluggable\SluggableServiceProvider');
-
+        $this->app->register('SleepingOwl\Admin\AdminServiceProvider');
+        $this->app->register('Illuminate\Html\HtmlServiceProvider');
+        $loader = AliasLoader::getInstance();
+        $loader->alias('SleepingOwl\Admin\Admin', 'Admin');
+        $loader->alias('SleepingOwl\AdminAuth\Facades\AdminAuth', 'AdminAuth');
+        $loader->alias('SleepingOwl\Admin\Facades\AdminRouter', 'AdminRouter');
+        $loader->alias('SleepingOwl\Admin\AssetManager\AssetManager', 'AssetManager');
+        $loader->alias('SleepingOwl\Admin\Columns\Column', 'Column');
+        $loader->alias('SleepingOwl\Admin\Models\Form\FormItem', 'FormItem');
+        $loader->alias('SleepingOwl\Admin\Models\ModelItem', 'ModelItem');
+        $loader->alias('Illuminate\Html\FormFacade', 'FormFacade');
+        $loader->alias('Illuminate\Html\HtmlFacade', 'HtmlFacade');
         include __DIR__ . '/routes.php';
     }
 
