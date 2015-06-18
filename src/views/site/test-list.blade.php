631e2878f6143dbb62bdd4d856f500117fb3c8b0
Andrey Degtyaruk <hlogeon1@gmail.com>
2015-06-18 14:16:28 +0700

    list layout fixes

--- src/views/site/test-list.blade.php
+++ src/views/site/test-list.blade.php
@@ -1,16 +1,25 @@
+<?php
+$routePrefix = config('scms.route_prefix') !== '' ? config('scms.route_prefix').'.' : '';
+$pageRoute = $routePrefix . config('scms.read.alias');
+?>
 @extends('scms::site.layouts.blog')
 
 
 @section('content')
     @foreach($models as $post)
         <article>
-            <div class="title"><a href="#">{{{$post->title}}}</a></div>
+            <div class="title"><a href="{{route($pageRoute, ['id' => $post->id, 'model' => $post->type->id])}}">{{{$post->title}}}</a></div>
+            @if($post->short_content)
             {!! $post->short_content !!}
+            @else
+                {{{str_limit(strip_tags($post->content), 250)}}}
+            @endif
+            <a href="{{route($pageRoute, ['id' => $post->id, 'model' => $post->type->id])}}">Читать далее</a>
             <div class="article-info">
                 @if($post->user)
                     <div class="person">
-                        @if($post->user->profile_image)
-                        <img src="{{$post->user->profile_image->thumbnail('small')}}" height="40" width="40">
+                        @if($post->user->profile_image->thumbnail('small'))
+                        <img src="{!! $post->user->profile_image->thumbnail('small') !!}" height="40" width="40">
                         @else
                             <img src="{{asset('/img/photo.png')}}" height="40" width="40">
                         @endif
